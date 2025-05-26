a13333dcbc841f48a6dfdded16296047eaa43106
Raghav-chandak <raghavchandak27@gmail.com>
2025-05-26 23:59:28 +0530

    conditions

---
+++ Grade calculator.py
@@ -0,0 +1,11 @@
+score=int(input("Enter your score:"))
+if 90<score<100:
+    print("You got grade A.")
+elif 80<score<89:
+    print("You got grade B.")
+elif 70<score<79:
+    print("You got grade C.")
+elif 60<score<69:
+    print("You got grade D.")
+else:
+    print("You got grade E.")
