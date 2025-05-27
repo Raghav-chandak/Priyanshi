2fce30a1f2cbfe8eb482b55b382a25ad0cff8c82
Raghav-chandak <raghavchandak27@gmail.com>
2025-05-27 23:24:43 +0530

    Conditions

---
+++ Fruit ripeness checker.py
@@ -0,0 +1,11 @@
+fruit=input("Enter fruit:")
+if fruit=="banana":
+    colour=input("Enter the colour your fruit:")
+    if colour=="Yellow":  
+        print("The fruit is ripe.")
+    elif colour=="green":
+        print("The fruit is unripped.")   
+    elif colour=="Brown":
+        print("The fruit is overipped.")
+else :
+    print("False")
---
+++ Weather activity suggestion.py
@@ -0,0 +1,9 @@
+weather=input("What's the weather:")
+if weather == "Sunny":
+    print("You can go for a walk.")
+elif weather == "Rainy":
+    print("You can read a book.")   
+elif weather == "Snowy":
+    print("You can build a snowman.")
+else :
+    print("False")
